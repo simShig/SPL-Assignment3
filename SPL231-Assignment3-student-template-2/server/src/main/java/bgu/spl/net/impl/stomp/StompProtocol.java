cebd210f7fb0fa970a076283e90f3f9c0f472c4c
simShig <simshigol@gmail.com>
2023-01-04 21:52:41 +0200

    adding to StompProtocol - inspired by RCIprotocol

--- SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/impl/stomp/StompProtocol.java
+++ SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/impl/stomp/StompProtocol.java
@@ -5,22 +5,31 @@ Based on the EchoProtocol and interface StompMassagingProtocol
 package bgu.spl.net.impl.stomp;
 
 import bgu.spl.net.api.StompMessagingProtocol;
+import bgu.spl.net.impl.newsfeed.NewsFeed;
+
 import java.time.LocalDateTime;
 
 public class StompProtocol implements StompMessagingProtocol<String> {
 
     private boolean shouldTerminate = false;
+    private NewsFeed NewsDataStructure; 
+    private ConnectionsImpl ConnectionsDataStructure;  
     
     // public void start(int connectionId, Connections<String> connections){   //someone in the group said Hadi allowed to delete it
     //     return;
     // };
+    public StompProtocol (NewsFeed NDS, ConnectionsImpl CDS){   //CONSTRUCTOR - inspired by RCIprotocol
+        NewsDataStructure = NDS;
+        ConnectionsDataStructure = CDS;
+    }
     
-    public void process(String message){
-        return;
+    public void process(String message){           //SMP interface method
+        //message PARSE method                  //TODO - method to parse the massage 
+        return ((Command) msg).execute(arg);        //TODO - 
     };
 	
 
-    public boolean shouldTerminate(){
+    public boolean shouldTerminate(){           // SMP interface method
         shouldTerminate = true;
         return shouldTerminate;
 
