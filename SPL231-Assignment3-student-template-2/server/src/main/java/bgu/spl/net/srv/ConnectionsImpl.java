27612748bce73a1840aa225e62f009766523c979
simShig <simshigol@gmail.com>
2023-01-04 21:57:34 +0200

    implemented ConnectionsImpl skelaton

---
+++ SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -0,0 +1,22 @@
+package bgu.spl.net.srv;
+
+public class ConnectionsImpl implements Connections<String>{
+
+
+    //TODO - Implement this class entirely - only implemented skelaton so i can referance Type ConnectionsImpl as a Type.
+
+    public boolean send(int connectionId, String msg){
+
+            return false;
+    };
+
+    public void send(String channel, String msg){
+
+    };
+
+    public void disconnect(int connectionId){
+
+    };
+
+
+}
