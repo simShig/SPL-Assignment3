2ebf455b349729b8b70560358095b16f956109f8
simShig <simshigol@gmail.com>
2023-01-06 10:39:35 +0200

    transfer string2frame and the other one to stopmProtocol

--- SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/impl/stomp/FrameFormat.java
+++ SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/impl/stomp/FrameFormat.java
@@ -13,8 +13,8 @@ String FrameBody = null;
 char EndOfMassage = '\u0000';
 
 //delimiters for transfering to\from string
-String EndOfLine = "\n";
-String EndOfField = "\n\ff";    //end of field is also the end of a line
+static String EndOfLine = "\n";
+static String EndOfField = "\n\ff";    //end of field is also the end of some line
 
 
 //methods:
@@ -25,44 +25,45 @@ public FrameFormat (String stompCmd,LinkedList<LinkedList<String>> stompHDRS,Str
 }
 
 
-public String frame2String (){
-    String ans=null;
-//add commandHeader:
-    ans=ans+stompCommand + EndOfField;
+// public String frame2String (){
+//     String ans=null;
+// //add commandHeader:
+//     ans=ans+stompCommand + EndOfField;
 
-//add stompHeaders:
-    for (LinkedList<String> header : stompHeaders) {
-        ans=ans+header.get(0) + ":"+header.get(1) + EndOfLine;
-    }
-    ans+=EndOfField;
+// //add stompHeaders:
+//     for (LinkedList<String> header : stompHeaders) {
+//         ans=ans+header.get(0) + ":"+header.get(1) + EndOfLine;
+//     }
+//     ans+=EndOfField;
 
-//add body:
-    ans+= FrameBody+EndOfField;
-//add EOM:
-    ans+=EndOfMassage;
-    return ans;
-}
+// //add body:
+//     ans+= FrameBody+EndOfField;
+// //add EOM:
+//     ans+=EndOfMassage;
+//     return ans;
+// }
 
-public FrameFormat string2Frame (String str){               //transforms msg back to frame
-    String[] splitByFields = str.split(EndOfField);
-    //commandHeader:
-    String sCommand = splitByFields[0];
-    //stompHeaders:
-    LinkedList<LinkedList<String>> sHeaders = new LinkedList<>();
-    for (int i = 1; i < splitByFields.length-1; i++) {
-      String headerName = splitByFields[i].split(":")[0];
-      String headerValue = splitByFields[i].split(":")[1];
-      LinkedList<String> Pair = new LinkedList<>();
-      Pair.add(headerName);
-      Pair.add(headerValue);
-      sHeaders.add(Pair);
-    }
-    //body:
-    String sBody = splitByFields[splitByFields.length-1];
+// public FrameFormat string2Frame (String str){               //transforms msg back to frame
+//     String[] splitByFields = str.split(EndOfField);
+//     //commandHeader:
+//     String sCommand = splitByFields[0];
+//     //stompHeaders:
+//     LinkedList<LinkedList<String>> sHeaders = new LinkedList<>();
+//     String[] headersSplitByEOL = splitByFields[1].split(EndOfLine);
+//     for (int i = 1; i < headersSplitByEOL.length-1; i++) {
+//       String headerName = headersSplitByEOL[i].split(":")[0];
+//       String headerValue = headersSplitByEOL[i].split(":")[1];
+//       LinkedList<String> Pair = new LinkedList<>();
+//       Pair.add(headerName);
+//       Pair.add(headerValue);
+//       sHeaders.add(Pair);
+//     }
+//     //body:
+//     String sBody = splitByFields[2];
 
-    FrameFormat recievedFrame = new FrameFormat(sCommand, sHeaders, sBody);
-    return recievedFrame;
-}
+//     FrameFormat recievedFrame = new FrameFormat(sCommand, sHeaders, sBody);
+//     return recievedFrame;
+// }
 
 
 
---
+++ SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/impl/stomp/StompProtocolImpl.java
@@ -0,0 +1,109 @@
+/*
+Based on the EchoProtocol and interface StompMassagingProtocol
+ */
+
+package bgu.spl.net.impl.stomp;
+
+import bgu.spl.net.api.StompMessagingProtocol;
+import bgu.spl.net.impl.newsfeed.NewsFeed;
+import bgu.spl.net.impl.rci.Command;
+import bgu.spl.net.srv.ConnectionsImpl;
+import java.util.LinkedList;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class StompProtocolImpl implements StompMessagingProtocol<String> {
+
+    private String EndOfLine = FrameFormat.EndOfLine;
+    private String EndOfField = FrameFormat.EndOfField;
+    private boolean shouldTerminate = false;
+    // private NewsFeed NewsDataStructure; 
+    // private ConnectionsImpl ConnectionsDataStructure;  
+    
+    // public void start(int connectionId, Connections<String> connections){   //someone in the group said Hadi allowed to delete it
+    //     return;
+    // };
+    public StompProtocolImpl (NewsFeed NDS, ConnectionsImpl CDS){   //CONSTRUCTOR - inspired by RCIprotocol
+        // NewsDataStructure = NDS;
+        // ConnectionsDataStructure = CDS;
+    }
+    
+    public void process(String  msg){           //SMP interface method
+        //message PARSE() method                  //TODO - method to parse the massage 
+        FrameFormat recievedFrame =string2Frame(msg);
+        
+
+
+        //return ((Command) message).execute(NewsDataStructure,ConnectionsDataStructure);        //TODO - change to fit XxxxxCommand.execute (as we designed)
+    };
+	
+
+    public boolean shouldTerminate(){           // SMP interface method
+        shouldTerminate = true;
+        return shouldTerminate;
+
+    };
+
+
+    public FrameFormat string2Frame (String str){               //transforms msg back to frame
+        String[] splitByFields = str.split(EndOfField);
+        //commandHeader:
+        String sCommand = splitByFields[0];
+        //stompHeaders:
+        LinkedList<LinkedList<String>> sHeaders = new LinkedList<>();
+        String[] headersSplitByEOL = splitByFields[1].split(EndOfLine);
+        for (int i = 1; i < headersSplitByEOL.length-1; i++) {
+          String headerName = headersSplitByEOL[i].split(":")[0];
+          String headerValue = headersSplitByEOL[i].split(":")[1];
+          LinkedList<String> Pair = new LinkedList<>();
+          Pair.add(headerName);
+          Pair.add(headerValue);
+          sHeaders.add(Pair);
+        }
+        //body:
+        String sBody = splitByFields[2];
+    
+        FrameFormat recievedFrame = new FrameFormat(sCommand, sHeaders, sBody);
+        return recievedFrame;
+    }
+    public String frame2String (FrameFormat frame){
+        String ans=null;
+    //add commandHeader:
+        ans=ans+frame.stompCommand + EndOfField;
+    
+    //add stompHeaders:
+        for (LinkedList<String> header : frame.stompHeaders) {
+            ans=ans+header.get(0) + ":"+header.get(1) + EndOfLine;
+        }
+        ans+=EndOfField;
+    
+    //add body:
+        ans+= frame.FrameBody+EndOfField;
+    //add EOM:
+        ans+=frame.EndOfMassage;
+        return ans;
+    }
+
+    /*
+ * EchoProtocol:
+ * 
+ * 
+ @Override
+ public String process(String msg) {
+     shouldTerminate = "bye".equals(msg);
+     System.out.println("[" + LocalDateTime.now() + "]: " + msg);
+     return createEcho(msg);
+    }
+    
+    private String createEcho(String message) {
+        String echoPart = message.substring(Math.max(message.length() - 2, 0), message.length());
+        return message + " .. " + echoPart + " .. " + echoPart + " ..";
+    }
+    
+    @Override
+    public boolean shouldTerminate() {
+        return shouldTerminate;
+    }
+    */
+}
