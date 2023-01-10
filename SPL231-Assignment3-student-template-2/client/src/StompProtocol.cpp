3c56977d1731ef26119052151f2b82f3eb035e37
edenmic <denush196@gmail.com>
2023-01-10 20:56:48 +0200

    trying again

--- SPL231-Assignment3-student-template-2/client/src/StompProtocol.cpp
+++ SPL231-Assignment3-student-template-2/client/src/StompProtocol.cpp
@@ -1,10 +1,31 @@
 #pragma once
 
 #include "../include/StompProtocol.h"
-
+#include "../include/FrameFormat.h"
+#include <stdlib.h>
+#include "../include/ConnectionHandler.h"
+#include "../include/StompClient.h"
 // TODO: implement the STOMP protocol
 class StompProtocol
 {
 private:
 public:
+
+
+string StompProtocol::process(string message)
+{
+    
+}
+
+FrameFormat StompProtocol::string2Frame(string line)
+{
+ 
+}
+
+string StompProtocol::Frame2String(FrameFormat frame)
+{
+    
+}
+
+
 };
