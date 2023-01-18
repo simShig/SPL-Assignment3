144e598e17203ac3f60fef97332a9ae564f0e3da
edenmic <denush196@gmail.com>
2023-01-18 04:31:46 +0200

    yay

--- SPL231-Assignment3-student-template-2/client/include/StompProtocol.h
+++ SPL231-Assignment3-student-template-2/client/include/StompProtocol.h
@@ -1,10 +1,16 @@
 #pragma once
 #include "../include/ConnectionHandler.h"
 #include "../include/FrameFormat.h"
+#include "event.h"
+#include "summary.h"
 #include <unordered_map>
 #include <string>
 #include <vector>
-
+#include <map>
+#include <deque>
+using std::string;
+using namespace std;
+class Summary;
 
 // TODO: implement the STOMP protocol
 class StompProtocol
@@ -21,12 +27,18 @@ private:
     std::string handleLogin(std::vector<std::string> &splitedFrame);
     std::string handleJoin(std::vector<std::string> &splitedFrame);
     std::string handleExit(std::vector<std::string> &splitedFrame);
-    std::string handleReport(std::vector<std::string> &splitedFrame);
+    std::string handleReport(std::vector<std::string> &splitedFrame, std::string &dataFromUser);
     std::string handleLogout(std::vector<std::string> &splitedFrame);
     std::vector<std::string> splitString(const std::string &str, char x);
     std::vector<std::string> sendFrame(std::vector<std::string> &splitedInput);
-    std::string StompProtocol::createEventFrame(Event &event, string topic);
+    std::string StompProtocol::createEventFrame(Event &event, string topic); /////////////////////
 
+    int subCounter;
+    int recieptCounter;
+    std::string userName;
+    std::map<string, string> subScribed; // topic - id
+    std::map<string, std::map<string, Summary>> summaries;
+    std::map<string, string> recieptMap; // reciept - action
 
 public:
     string process(string message);
@@ -35,5 +47,37 @@ public:
     StompProtocol();
     bool shouldTerminate();
     void stompToString(std::string &stompFrame);
-    std::string stringToStomp(std::string &dataFromUser);
+    std::string stringToStomp(std::string &dataFromUser); /////////////////
+
+    vector<string> processUser(string);
+    void processLogIn(string s, ConnectionHandler &connectionHandler);
+    string processServer(string);
+    string transmit(string c, std::map<string, string> h, string b);
+    string hashMapToString(std::map<string, string> h);
+    string createFrameEvent(Event e);
+    vector<string> createVector(names_and_events parsed);
+    void addEvents(names_and_events parsed);
+    bool isSummary(string s);
+    string summary(string s);
+    void send(std::vector<string> lines);
+    bool logIn(string s);
+    string CONNECTED(string s);
+    string ERROR(string s);
+    string MESSEGE(string s);
+    string RECIEPT(string s);
+    std::map<std::string, std::string> mappify2(std::string const &s);
+    bool GameSummaryNotContainsUser(string userName, string gameName);
+    bool summariesNotContainGame(string gameName);
+    void addGameToSummaries(string gameName);
+    void addUserNameToGameSummary(string userName, string gameName);
+    void checksBeforeAddEvent(string userName, string gameName);
+    string logoutFrame();
+
+    // for the event constructure from procces
+    string get_user_name(std::deque<std::string> &s);
+    void addingEvent(Event &e, string userid);
+    void logout();
+    std::string StompProtocol::ans2Frame(std::vector<std::string> ans);
+
 };
+
--- SPL231-Assignment3-student-template-2/client/src/StompProtocol.cpp
+++ SPL231-Assignment3-student-template-2/client/src/StompProtocol.cpp
@@ -1,4 +1,3 @@
-
 #ifndef STOMPPROTOCOL_CPP
 #define STOMPPROTOCOL_CPP
 
@@ -13,190 +12,224 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 // TODO: implement the STOMP protocol
 StompProtocol ::StompProtocol() : recipt_id_counter(1), game_id_counter(1), should_terminate(false), id_to_game(), game_to_id(), receipt_id_to_message() {}
 
 bool StompProtocol::shouldTerminate()
 {
-    return should_terminate;
+ return should_terminate;
 }
 
 void StompProtocol ::stompToString(std::string &stompFrame)
 {
-    // std::cout << "inside StompProtocol::stompToString";
-    std::stringstream ss(stompFrame);
-    std::string frameHeadLine;
-    std::getline(ss, frameHeadLine, ';');
-    //std::cout << "====headline is:===="+frameHeadLine + "===stompFrame is: ===" +stompFrame + "***************\n";
+ // std::cout << "inside StompProtocol::stompToString";
+ std::stringstream ss(stompFrame);
+ std::string frameHeadLine;
+ std::getline(ss, frameHeadLine, ';');
+ //std::cout << "====headline is:===="+frameHeadLine + "===stompFrame is: ===" +stompFrame + "***************\n";
 
-    if (frameHeadLine == "CONNECTED")
-    {
-    //std::cout << ">>>>>>>>>>>>>>>>"+stompFrame;
+ if (frameHeadLine == "CONNECTED")
+ {
+ //std::cout << ">>>>>>>>>>>>>>>>"+stompFrame;
 
-        return;
-    }
-    else if (frameHeadLine == "RECEIPT")
-    {
-        StompProtocol::handleReceipt(stompFrame);
-    }
-    else if (frameHeadLine == "MESSAGE")
-    {
-        StompProtocol::handleMassage(stompFrame);
-    }
-    else if (frameHeadLine == "ERROR")
-    {
-    std::cout << "inside ~if ERROR~: \n";
-        std::cout << stompFrame;
-    }
-    else
-    {
-        return;
-        //std::cout << "what the hell???@?";
-    }
+ return;
+ }
+ else if (frameHeadLine == "RECEIPT")
+ {
+ StompProtocol::handleReceipt(stompFrame);
+ }
+ else if (frameHeadLine == "MESSAGE")
+ {
+ StompProtocol::handleMassage(stompFrame);
+ }
+ else if (frameHeadLine == "ERROR")
+ {
+ std::cout << "inside ~if ERROR~: \n";
+ std::cout << stompFrame;
+ }
+ else
+ {
+ return;
+ //std::cout << "what the hell???@?";
+ }
 }
 
 void StompProtocol ::handleReceipt(std::string &stompFrame)
 {
-    std::vector<std::string> splitedFrame = splitString(stompFrame, ';');
-    std::string receiptID = splitedFrame[1];
-    std::string content = receipt_id_to_message[receiptID];
-    if (content == "disconnect")
-    {
-        std ::cout << "Bye Bye <3" << std::endl;
-        should_terminate = true;
-    }
-    else
-    {
-        std::cout << content << std::endl;
-    }
+ std::vector<std::string> splitedFrame = splitString(stompFrame, ';');
+ std::string receiptID = splitedFrame[1];
+ std::string content = receipt_id_to_message[receiptID];
+ if (content == "disconnect")
+ {
+ std ::cout << "Bye Bye <3" << std::endl;
+ should_terminate = true;
+ }
+ else
+ {
+ std::cout << content << std::endl;
+ }
 }
 
 void StompProtocol ::handleMassage(std::string &stompFrame)
 {
-    std::cout << stompFrame << std::endl;
+ std::cout << stompFrame << std::endl;
 }
 
 std::vector<std::string> StompProtocol::splitString(const std::string &str, char x)
 {
-    std::stringstream ss(str);
-    std::string line;
-    std::vector<std::string> split_str;
-    while (std::getline(ss, line, x))
-    {
-        split_str.push_back(line);
-    }
-    return split_str;
+ std::stringstream ss(str);
+ std::string line;
+ std::vector<std::string> split_str;
+ while (std::getline(ss, line, x))
+ {
+ split_str.push_back(line);
+ }
+ return split_str;
 }
 
 //______________________________________________________
 
 std::string StompProtocol::stringToStomp(std::string &dataFromUser)
 {
-    std::vector<std::string> splitedFrame = splitString(dataFromUser, ' ');
-    std::string action = splitedFrame[0];
-    if (action == "login")
-    {
-        // std::cout << "im in login if, ---" + splitedFrame[0] + "--- is splitedFrame0, 1---" + splitedFrame[1] 
-        // + "--- 2---" + splitedFrame[2]+ "--- 3---" + splitedFrame[3]<< std::endl;
-        return handleLogin(splitedFrame);
-    }
-    else if (action == "join")
-    {
-        return handleJoin(splitedFrame);
-    }
-    else if (action == "exit")
-    {
-        return handleExit(splitedFrame);
-    }
-    else if (action == "report")
-    {
-        return handleReport(splitedFrame);
-    }
-    else if (action == "logout")
-    {
-        return handleLogout(splitedFrame);
-    }
-    else
-    {
-        std:: cout<<"illigal command"<<std::endl;
-        return "%";
-    }
+ std::vector<std::string> splitedFrame = splitString(dataFromUser, ' ');
+ std::string action = splitedFrame[0];
+ if (action == "login")
+ {
+ // std::cout << "im in login if, ---" + splitedFrame[0] + "--- is splitedFrame0, 1---" + splitedFrame[1] 
+ // + "--- 2---" + splitedFrame[2]+ "--- 3---" + splitedFrame[3]<< std::endl;
+ return handleLogin(splitedFrame);
+ }
+ else if (action == "join")
+ {
+ return handleJoin(splitedFrame);
+ }
+ else if (action == "exit")
+ {
+ return handleExit(splitedFrame);
+ }
+ else if (action == "report")
+ {
+ return handleReport(splitedFrame, dataFromUser);
+ }
+ else if (action == "logout")
+ {
+ return handleLogout(splitedFrame);
+ }
+ else
+ {
+ std:: cout<<"illigal command"<<std::endl;
+ return "%";
+ }
 }
 
 std::string StompProtocol::handleLogin(std::vector<std::string> &splitedFrame)
 {
-    //std::cout << "im in handleLogin: \n" << std::endl;
-    if (splitedFrame.size() != 4)
-    {
-        //std::cout << "im in if (splitedFrame.size() != 4): \n" << std::endl;
-        std::cout << "illigal command, login dont have 4 words" << std::endl;
-        return "%";
-    }
-    else
-    {
-        //std::cout << "im in if ELSE HANDLELOGIN: \n" << std::endl;
-        receipt_id_to_message[std::to_string(recipt_id_counter)] = "Login successful";
-        std::string frame = "CONNECT;F;receipt-id:" + std::to_string(recipt_id_counter) + ";L;" + "accept-version:1.2;L;host:" + splitedFrame[1] + ";L;login:" + splitedFrame[2] + ";L;passcode:" + splitedFrame[3] + ";F; emptybodie";
-        recipt_id_counter += 1;
-        //std::cout << frame << std::endl;
-        std::cout << "Login successful" << std::endl;
-        return frame;
-    }
+ //std::cout << "im in handleLogin: \n" << std::endl;
+ if (splitedFrame.size() != 4)
+ {
+ //std::cout << "im in if (splitedFrame.size() != 4): \n" << std::endl;
+ std::cout << "illigal command, login dont have 4 words" << std::endl;
+ return "%";
+ }
+ else
+ {
+ //std::cout << "im in if ELSE HANDLELOGIN: \n" << std::endl;
+ receipt_id_to_message[std::to_string(recipt_id_counter)] = "Login successful";
+ std::string frame = "CONNECT;F;receipt-id:" + std::to_string(recipt_id_counter) + ";L;" + "accept-version:1.2;L;host:" + splitedFrame[1] + ";L;login:" + splitedFrame[2] + ";L;passcode:" + splitedFrame[3] + ";F; emptybodie";
+ recipt_id_counter += 1;
+ //std::cout << frame << std::endl;
+ std::cout << "Login successful" << std::endl;
+ return frame;
+ }
 }
 
 std::string StompProtocol::handleJoin(std::vector<std::string> &splitedFrame)
 {
-    if (splitedFrame.size() != 2)
-    {
-        std::cout << "illigal command, join dont have 2 words" << std::endl;
-        return "%";
-    }
-    else
-    {
-        id_to_game[std::to_string(game_id_counter)] = splitedFrame[1];
-        game_to_id[splitedFrame[1]] = std::to_string(game_id_counter);
-        receipt_id_to_message[std::to_string(recipt_id_counter)] = "Joined channel " + splitedFrame[1];
-        std::string frame = "SUBSCRIBE;F;receipt-id:" + std::to_string(recipt_id_counter) + ";L;destination:" + splitedFrame[1] + ";L;id:" + std::to_string(game_id_counter) + ";F;  ";
-        recipt_id_counter += 1;
-        game_id_counter += 1;
-        std::cout << "Joined channel " + splitedFrame[1] << std::endl;
-        return frame;
-    }
+ if (splitedFrame.size() != 2)
+ {
+ std::cout << "illigal command, join dont have 2 words" << std::endl;
+ return "%";
+ }
+ else
+ {
+ id_to_game[std::to_string(game_id_counter)] = splitedFrame[1];
+ game_to_id[splitedFrame[1]] = std::to_string(game_id_counter);
+ receipt_id_to_message[std::to_string(recipt_id_counter)] = "Joined channel " + splitedFrame[1];
+ std::string frame = "SUBSCRIBE;F;receipt-id:" + std::to_string(recipt_id_counter) + ";L;destination:" + splitedFrame[1] + ";L;id:" + std::to_string(game_id_counter) + ";F; ";
+ recipt_id_counter += 1;
+ game_id_counter += 1;
+ std::cout << "Joined channel " + splitedFrame[1] << std::endl;
+ return frame;
+ }
 }
 
 std::string StompProtocol::handleExit(std::vector<std::string> &splitedFrame)
 {
-    if (splitedFrame.size() != 2)
-    {
-        std::cout << "illigal command, exit dont have 2 words" << std::endl;
-        return "%";
-    }
-    else
-    {
-        std ::string game_id = game_to_id[splitedFrame[1]];
-        id_to_game.erase(game_id);
-        game_to_id.erase(splitedFrame[1]);
-        receipt_id_to_message[std::to_string(recipt_id_counter)] = "Exited channel " + splitedFrame[1];
-        std::string frame = "UNSUBSCRIBE;F;receipt-id:" + std::to_string(recipt_id_counter) + ";L;destination:" + splitedFrame[1] + ";L;ID:" + game_id + ";F;   ";
-        recipt_id_counter += 1;
-        std::cout << "Exited channel " + splitedFrame[1] << std::endl;
-        return frame;
-    }
+ if (splitedFrame.size() != 2)
+ {
+ std::cout << "illigal command, exit dont have 2 words" << std::endl;
+ return "%";
+ }
+ else
+ {
+ std ::string game_id = game_to_id[splitedFrame[1]];
+ id_to_game.erase(game_id);
+ game_to_id.erase(splitedFrame[1]);
+ receipt_id_to_message[std::to_string(recipt_id_counter)] = "Exited channel " + splitedFrame[1];
+ std::string frame = "UNSUBSCRIBE;F;receipt-id:" + std::to_string(recipt_id_counter) + ";L;destination:" + splitedFrame[1] + ";L;ID:" + game_id + ";F; ";
+ recipt_id_counter += 1;
+ std::cout << "Exited channel " + splitedFrame[1] << std::endl;
+ return frame;
+ }
 }
 
-std::string StompProtocol::handleReport(std::vector<std::string> &splitedFrame)
+std::string StompProtocol::handleReport(std::vector<std::string> &splitedFrame, std::string &dataFromUser)
 {
-    return sendFrame();
+ string file;
+ std::stringstream input_stringstream(dataFromUser);
+ getline(input_stringstream, file, ' ');
+ file = "./data/" + file;
+ names_and_events parsed = parseEventsFile(file);
+ addEvents(parsed);
+ std::vector<std::string> ans;
+ ans = createVector(parsed);
+ string frame = ans2Frame(ans);
+ return frame; //TODO
+}
+std::string StompProtocol::ans2Frame(std::vector<std::string> ans)
+{
+ std::string frame;
+ for (const std::string& str : ans) {
+ frame.append(str + ";L;");
+ }
+ std::cout << frame << std::endl;
+ return frame;
+ 
 }
 
+void StompProtocol::addEvents(names_and_events parsed)
+{
+ for (Event e : parsed.events)
+ {
+ string gameName = e.get_team_a_name() + "_" + e.get_team_b_name();
+ checksBeforeAddEvent(userName, gameName);
+ if (subScribed.count(gameName) == 0)
+ {
+ should_terminate=true;
+ break;
+ }
+ (summaries[gameName])[userName].addEvent(e); // add event to the summary of the user
+ }
+}
 
 std::string StompProtocol::handleLogout(std::vector<std::string> &splitedFrame)
 {
-    receipt_id_to_message[std::to_string(recipt_id_counter)] = "disconnect";
-    std::string frame = "DISCONNECT;F;receipt:" + std::to_string(recipt_id_counter) +";F;  ";
-    recipt_id_counter += 1;
-    return frame;
+ receipt_id_to_message[std::to_string(recipt_id_counter)] = "disconnect";
+ std::string frame = "DISCONNECT;F;receipt:" + std::to_string(recipt_id_counter) +";F; ";
+ recipt_id_counter += 1;
+ return frame;
 }
 //=================================================================
 std::vector<std::string> StompProtocol::sendFrame(std::vector<std::string> &splitedInput)
@@ -224,28 +257,163 @@ teamAUpdates += key.first + string(": ") + key.second + ";L;";
 for (auto key : event.get_team_b_updates())
 teamBUpdates += key.first + string(": ") + key.second + ";L;";
 string frame = "SEND;L;" + string("destination:/topic/") + topic + ";L;" + "receipt-id:" + std::to_string(recipt_id_counter) + ";L;" + ";L;" +
-  string("user: ") +  event.get_name() + ";L;" + string("team a: ") + event.get_team_a_name() + ";L;" + string("team b: ") +
-  event.get_team_b_name() + ";L;" + string("event name: ") + event.get_name() +
-  ";L;" + string("time: ") + std::to_string(event.get_time()) + ";L;" + string("general game updates: ") +
-  generalUpdates + ";L;" + string("team a updates: ") + teamAUpdates + ";L;" + string("team b updates: ") +
-  teamBUpdates + ";L;" + string("description: ") + event.get_discription() + ";L;;F;";
+ string("user: ") + event.get_name() + ";L;" + string("team a: ") + event.get_team_a_name() + ";L;" + string("team b: ") +
+ event.get_team_b_name() + ";L;" + string("event name: ") + event.get_name() +
+ ";L;" + string("time: ") + std::to_string(event.get_time()) + ";L;" + string("general game updates: ") +
+ generalUpdates + ";L;" + string("team a updates: ") + teamAUpdates + ";L;" + string("team b updates: ") +
+ teamBUpdates + ";L;" + string("description: ") + event.get_discription() + ";L;;F;";
 return frame;
 }
 
-// string StompProtocol::process(string message)
-// {
-    
-// }
+//----------------------------------------------------------------------------
 
-// FrameFormat StompProtocol::string2Frame(string line)
-// {
- 
-// }
+string StompProtocol::summary(string s)
+{
+ string gameName;
+ stringstream input_stringstream(s);
+ getline(input_stringstream, gameName, ' ');
+ getline(input_stringstream, gameName, ' '); // twice to skip "summary"
+ string userName;
+ getline(input_stringstream, userName, ' ');
+ string file;
+ getline(input_stringstream, file, ' ');
+ // need to check if the user and the game exist ###
+ return (summaries[gameName])[userName].toString(file);
+}
+bool StompProtocol::isSummary(string s)
+{
+ stringstream input_stringstream(s);
+ string word;
+ getline(input_stringstream, word, ' ');
+ return word == "summary";
+}
+void StompProtocol::addEvents(names_and_events parsed)
+{
+ for (Event e : parsed.events)
+ {
+ string gameName = e.get_team_a_name() + "_" + e.get_team_b_name();
+ checksBeforeAddEvent(userName, gameName);
+ if (subScribed.count(gameName) == 0)
+ {
+ should_terminate=true;
+ break;
+ }
+ (summaries[gameName])[userName].addEvent(e); // add event to the summary of the user
+ }
+}
+void StompProtocol::checksBeforeAddEvent(string userName, string gameName)
+{
+ if (summariesNotContainGame(gameName))
+ {
+ addGameToSummaries(gameName);
+ addUserNameToGameSummary(userName, gameName);
+ }
+ else if (GameSummaryNotContainsUser(userName, gameName))
+ {
+ addUserNameToGameSummary(userName, gameName);
+ }
+}
+bool StompProtocol::GameSummaryNotContainsUser(string userName, string gameName)
+{
+ return (summaries[gameName]).count(userName) == 0;
+}
+bool StompProtocol::summariesNotContainGame(string gameName)
+{
+ return summaries.count(gameName) == 0;
+}
+void StompProtocol::addGameToSummaries(string gameName)
+{
+ std::map<std::string, Summary> map; // create new map for the game
+ summaries[gameName] = map; // add to summaries the map
+}
+void StompProtocol::addUserNameToGameSummary(string userName, string gameName)
+{
+ Summary s; // create summay for the user that reported
+ (summaries[gameName])[userName] = s; // add the summary to the user in the map
+}
+vector<string> StompProtocol::createVector(names_and_events parsed)
+{
+ vector<string> ans;
+ for (Event e : parsed.events)
+ {
+ ans.push_back(createFrameEvent(e));
+ }
+ return ans;
+}
+string StompProtocol::createFrameEvent(Event e)
+{
+ string command = "SEND";
+ string topic = e.get_team_a_name() + "_" + e.get_team_b_name();
+ map<string, string> frameMap; // the frame headers
+ frameMap["destination"] = topic;
+ frameMap["reciept"] = to_string(recieptCounter);
+ recieptMap[to_string(recieptCounter)] = "sent";
+ recieptCounter++;
+ string b = "";
+ map<string, string> map;
+ b += "user:" + userName + "\n";
+ map["team a"] = e.get_team_a_name();
+ map["team b"] = e.get_team_b_name();
+ map["event name"] = e.get_name();
+ map["time"] = to_string(e.get_time());
+ b += hashMapToString(map);
+ b += "general Game updates: \n";
+ b += hashMapToString(e.get_game_updates());
+ b += "team a updates: \n";
+ b += hashMapToString(e.get_team_a_updates());
+ b += "team b updates: \n";
+ b += hashMapToString(e.get_team_b_updates());
+ b += "description: \n";
+ b += e.get_discription();
+ return transmit(command, frameMap, b);
+}
+string StompProtocol::hashMapToString(std::map<string, string> h)
+{
+ string message = "";
+ std::map<string, string>::iterator it = h.begin();
+ while (it != h.end())
+ {
+ message += it->first;
+ message += (":");
+ message += it->second;
+ message += ("\n");
+ it++;
+ }
+ return message;
+}
+string StompProtocol::transmit(string c, std::map<string, string> h, string b)
+{
+ string message(c);
+ message += ("\n");
+ std::map<string, string>::iterator it = h.begin();
+ while (it != h.end())
+ {
+ message += it->first;
+ message += (":");
+ message += it->second;
+ message += ("\n");
+ it++;
+ }
+ message += ("\n");
+ message += (b);
+ return message;
+}
 
-// string StompProtocol::Frame2String(FrameFormat frame)
-// {
-    
-// }
 
+void StompProtocol::addingEvent(Event &e, string userid)
+{
+ string gameName = e.get_team_a_name() + "_" + e.get_team_b_name();
+ checksBeforeAddEvent(userid, gameName);
+ (summaries[gameName])[userid].addEvent(e); // add event to the summary of the user
+}
+
+string StompProtocol::get_user_name(deque<std::string> &s)
+{ // get the user name from deque
+ string user_line = s.front();
+ int index = user_line.find(":");
+ string user_name = user_line.substr(index + 1);
+ s.pop_front(); // remove user name
+ return user_name;
+}
 
 #endif
